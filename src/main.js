bea91261dc86091811df8ef063b39a47a850c6be
prozactool <prozaconsixstrings@gmail.com>
2017-06-01 12:10:52 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// import notification from './notification';
+//
+//
+// notification.log('here we go');
+//
+// notification.announce('test');
+require('./main.scss');
+
+class Form {
+  constructor() {
+    // alert('yay, compiling from es2015')
+    let numbers = [5,10,15,20].map(number => number * 2);
+
+    console.log(numbers);
+
+  }
+}
+
+new Form();
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+
+const path = require('path');
+
+var inProduction = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+
+  entry: './src/main.js',
+
+  output: {
+
+      path: path.resolve(__dirname, './dist'),
+
+      filename: 'bundle.js'
+
+  },
+  module:{
+
+    rules:[
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader','css-loader','sass-loader']
+      },
+
+      {
+
+        test:/\.css$/,
+
+        use: ['style-loader', 'css-loader']
+
+      },
+
+      {
+
+        test: /\.js$/,
+
+        exclude: /node_modules/,
+
+        loader: "babel-loader"
+
+      }
+
+    ]
+
+  },
+
+  plugins: []
+
+};
+
+
+if (inProduction) {
+
+  module.exports.plugins.push(
+
+    new webpack.optimize.UglifyJsPlugin()
+
+  );
+
+}
